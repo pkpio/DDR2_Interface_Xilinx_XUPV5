b518cc06942f111beb2b8b6746c7ad1f391f7b52
Praveen Kumar Pendyala <praveendath92@gmail.com>
2013-11-08 12:09:56 +0530

    Changed to MIG_36_1
    This one has a test bench too ! Not tested on board yet.

--- coregen/mig_36_1/user_design/synth/mem_interface_top_synp.sdc
+++
@@ -1,4 +0,0 @@
-# Clock 
-define_clock -name {n:u_ddr2_infrastructure.sys_clk_ibufg} -period 3.75
-define_clock -name {n:u_ddr2_infrastructure.clk200_ibufg}  -period 5.0
-
--- xupv5_mig_design/mig_23/example_design/par/create_ise.bat
+++
@@ -1,8 +0,0 @@
-# Following coregen commands to be uncommented  when the parameter DEBUG_EN is changed from 0 to 1 in ddr2_sdram.v/.vhd file.
-#coregen -b icon4_cg.xco
-#coregen -b vio_async_in96_cg.xco
-#coregen -b vio_async_in192_cg.xco
-#coregen -b vio_sync_out32_cg.xco
-#coregen -b vio_async_in100_cg.xco
-#del *.ncf
-pjcli -f set_ise_prop.txt
